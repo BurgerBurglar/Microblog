9262c547b590347272b72a68248cf81b18b5ed98
Shuo Tian <shuo.tian@jda.com>
2020-03-22 14:13:03 -0500

    Localized time with Moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 # import app.log_errors
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -14,6 +15,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 bootstap = Bootstrap(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = "login"
 mail = Mail(app)
